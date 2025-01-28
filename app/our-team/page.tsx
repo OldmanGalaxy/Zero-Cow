ab8aa08826cb7937e10f3394080e1ae20e59cc34
purv-kabaria <153810916+OldmanGalaxy@users.noreply.github.com>
2025-01-29 01:29:56 +0530

    fix: our team footer

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -3,35 +3,56 @@ import Navbar from "@/components/Navbar/Navbar";
 import Section1 from "@/components/OurTeam/Section1";
 import Section2 from "@/components/OurTeam/Section2";
 import Footer from "@/components/Footer/Footer";
+import MilkFlow from "@/components/FAQ/MilkFlow";
 
-function page() {
+function OurTeamPage() {
   return (
-    <div className="min-h-screen relative bg-dgreen">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div
-        className="min-h-screen relative"
-        style={{
-          background: "transparent",
-          backgroundImage:
-            "linear-gradient(to bottom, #118B50, #E3F0AF, #118B50)",
-          backgroundSize: "100% 100%",
-          backgroundPosition: "0 0",
-          backgroundRepeat: "no-repeat",
-          marginTop: "5rem",
-        }}>
-        <div className="absolute inset-0">
+
+      {/* Main content area */}
+      <main className="flex-grow relative mt-20">
+        {/* Background gradient and image */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            background: "linear-gradient(to bottom, #118B50, #E3F0AF, #5DB996)",
+          }}>
           <img
             src="/images/milk1_full.png"
             className="h-full w-full object-contain"
             alt="Milk background"
           />
         </div>
-        <Section1 />
-        <Section2 />
+
+        {/* Content sections with added padding */}
+        <div className="relative z-10 pb-32">
+          {" "}
+          {/* Added bottom padding */}
+          <Section1 />
+          {/* Added spacing between sections */}
+          <div className="h-24" /> {/* Spacer div */}
+          <Section2 />
+          {/* Extra space at the bottom to prevent collision with milk wave */}
+          <div className="h-32" /> {/* Bottom spacer */}
+        </div>
+      </main>
+
+      {/* Footer section with milk wave */}
+      <div className="relative">
+        {/* Milk wave container with increased height */}
+        <div className="absolute inset-x-0 top-0 h-48 transform -translate-y-full overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-lgreen opacity-50" />
+          <MilkFlow />
+        </div>
+
+        {/* Footer with gradient background */}
+        <div className="relative bg-gradient-to-b from-lgreen to-dgreen">
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </div>
   );
 }
 
-export default page;
+export default OurTeamPage;
